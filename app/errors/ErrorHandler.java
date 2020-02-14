9d2898215fa5137032e099625af0beafed943257
Sébastien Prunier <sebprunier@MacBook-Pro-3.local>
2020-02-14 17:07:12 +0100

    Initial commit

---
+++ app/errors/ErrorHandler.java
@@ -0,0 +1,56 @@
+package errors;
+
+import com.typesafe.config.Config;
+import play.Environment;
+import play.api.OptionalSourceMapper;
+import play.api.UsefulException;
+import play.api.routing.Router;
+import play.http.DefaultHttpErrorHandler;
+import play.mvc.Http.RequestHeader;
+import play.mvc.Result;
+import play.mvc.Results;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+@Singleton
+public class ErrorHandler extends DefaultHttpErrorHandler {
+
+    @Inject
+    public ErrorHandler(
+            Config config,
+            Environment environment,
+            OptionalSourceMapper sourceMapper,
+            Provider<Router> routes) {
+        super(config, environment, sourceMapper, routes);
+    }
+
+    protected CompletionStage<Result> onProdServerError(
+            RequestHeader request, UsefulException exception) {
+        return CompletableFuture.completedFuture(
+                Results.internalServerError("A server error occurred: " + exception.getMessage()));
+    }
+
+
+    @Override
+    protected CompletionStage<Result> onNotFound(RequestHeader request, String message) {
+        return CompletableFuture.completedFuture(
+                Results.notFound(views.html.error.render(request, "Page not found", "Sorry, page not found!"))
+        );
+    }
+
+    @Override
+    protected CompletionStage<Result> onOtherClientError(RequestHeader request, int statusCode, String message) {
+        return CompletableFuture.completedFuture(
+                Results.notFound(views.html.error.render(request, message, message))
+        );
+    }
+
+    protected CompletionStage<Result> onForbidden(RequestHeader request, String message) {
+        return CompletableFuture.completedFuture(
+                Results.forbidden("You're not allowed to access this resource."));
+    }
+}
