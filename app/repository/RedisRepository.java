a777e5fce21475f834b586fb3382b57f0c6962a8
CedricG44 <cedric-garcia@sfr.fr>
2020-02-18 14:27:57 +0100

    feat: 5 last viewed heroes

--- app/repository/RedisRepository.java
+++ app/repository/RedisRepository.java
@@ -4,13 +4,12 @@ import io.lettuce.core.RedisClient;
 import models.StatItem;
 import models.TopStatItem;
 import play.Logger;
-import utils.StatItemSamples;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.Arrays;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.stream.Collectors;
 
@@ -19,14 +18,12 @@ public class RedisRepository {
 
     private static Logger.ALogger logger = Logger.of("RedisRepository");
 
-
     private final RedisClient redisClient;
 
     @Inject
     public RedisRepository(RedisClient redisClient) {
         this.redisClient = redisClient;
     }
-
 
     public CompletionStage<Boolean> addNewHeroVisited(StatItem statItem) {
         logger.info("Hero visited " + statItem.name);
@@ -44,15 +41,27 @@ public class RedisRepository {
     }
 
     private CompletionStage<Long> addHeroAsLastVisited(StatItem statItem) {
-        // TODO
-        return CompletableFuture.completedFuture(1L);
+        logger.info("Add last visited hero");
+        return redisClient
+                .connect()
+                .async()
+                .zadd("viewed-heroes", -new Timestamp(new Date().getTime()).getTime(), statItem.toJson().toString())
+                .thenCombine(redisClient
+                        .connect()
+                        .async()
+                        .zremrangebyrank("viewed-heroes", 5, -1), (ladd, lrem) -> ladd);
     }
 
     public CompletionStage<List<StatItem>> lastHeroesVisited(int count) {
-        logger.info("Retrieved last heroes");
-        // TODO
-        List<StatItem> lastsHeroes = Arrays.asList(StatItemSamples.IronMan(), StatItemSamples.Thor(), StatItemSamples.CaptainAmerica(), StatItemSamples.BlackWidow(), StatItemSamples.MsMarvel());
-        return CompletableFuture.completedFuture(lastsHeroes);
+        logger.info("Retrieved last viewed heroes");
+        return redisClient
+                .connect()
+                .async()
+                .zrange("viewed-heroes", 0, count - 1)
+                .thenApply(hs -> hs
+                        .stream()
+                        .map(StatItem::fromJson)
+                        .collect(Collectors.toList()));
     }
 
     public CompletionStage<List<TopStatItem>> topHeroesVisited(int count) {
@@ -60,7 +69,7 @@ public class RedisRepository {
         return redisClient
                 .connect()
                 .async()
-                .zrevrangeWithScores("top-heroes", 0, count)
+                .zrevrangeWithScores("top-heroes", 0, count - 1)
                 .thenApply(hs -> hs
                         .stream()
                         .map(h -> new TopStatItem(StatItem.fromJson(h.getValue()), (long) h.getScore()))
