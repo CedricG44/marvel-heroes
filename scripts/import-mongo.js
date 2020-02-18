ac2a3f20467e46ae630bc0fdb539017a5d5aa2e1
Benjamin Rivard <benjamin.rivard@imt-altantique.net>
2020-02-18 09:19:36 +0100

    import mongo OK

--- scripts/import-mongo.js
+++ scripts/import-mongo.js
@@ -7,5 +7,28 @@ var mongoUrl = "mongodb://localhost:27017";
 const dbName = "marvel";
 const collectionName = "heroes";
 
-// TODO
-console.log("TODO ;-)");
+const insertHereos = (db, callback) => {
+  const collection = db.collection(collectionName);
+  const heroes = [];
+  fs.createReadStream("all-heroes.csv")
+    .pipe(csv())
+    .on("data", data => heroes.push(data))
+    .on("end", () => {
+      collection.insertMany(heroes, (err, result) => {
+        callback(result);
+      });
+      console.log("finished parsing heroes");
+    });
+};
+
+MongoClient.connect(mongoUrl, (err, client) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  }
+  const db = client.db(dbName);
+  insertHereos(db, result => {
+    console.log(`${result.insertedCount} hereos inserted`);
+    client.close();
+  });
+});
